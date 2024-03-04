1b353124b6d4d90aac34fb1dfd4e6b66a9aa2195
GeiseR98 <ipatovsergejj@yandex.ru>
2024-03-04 21:48:14 +0300

    feat: самое очевидное решение

---
+++ src/main/java/org/example/ConvertingToString.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/*
+самое очевидное решение
+ */
+public class ConvertingToString {
+
+    public boolean isPalindrome(int x) {
+        String xString = String.valueOf(x);
+        StringBuilder xRevers = new StringBuilder(xString).reverse();
+        return xString.contentEquals(xRevers);
+    }
+}
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,8 +4,9 @@ public class Solution {
     public static void main(String[] args) {
 
         WithoutArrayAndReverse withoutArrayAndReverse = new WithoutArrayAndReverse();
+        ConvertingToString convertingToString = new ConvertingToString();
 
-        int x = 1000021;
-        System.out.println(withoutArrayAndReverse.isPalindrome(x));
+        int x = 1211215;
+        System.out.println(convertingToString.isPalindrome(x));
     }
 }
